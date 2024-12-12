4ab86fe22b5a550d4430869fcee773ad936091f2
Zxy Ayoub <155308552+0xZxyab1@users.noreply.github.com>
2024-12-12 12:22:57 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -10,11 +10,19 @@ echo -e "\e[40m\e[33m[*]Installing requirements...! \e[0m"
 echo -e "\e[32m[!]Pliz mdir walu hta ysali kolshi !!\e[0m"
 pkg install root-repo
 clear
-pkg install -y termux-tools tsu python wpa-supplicant pixiewps iw
+pkg install -y termux-tools tsu python wpa-supplicant pixiewps iw ethtool
 pip install random os 
 clear
 chmod +x $HOME/Zxy-WPS/oneshot.py
 python $HOME/Zxy-WPS/banner.py
 echo -e "\e[32m[*] Requirements t instalaw NICE :> \e[0m"
-#sudo python oneshot.py -i wlan0
+echo "sudo python $HOME/Zxy-WPS/oneshot.py -i wlan0 -K -w --iface-down" > $HOME/zxy
+chmod +x $HOME/zxy 
+mv $HOME/zxy $PREFIX/bin
+echo "cat $HOME/Zxy-WPS/reports/stored.txt" > $HOME/lista
+chmod +x $HOME/lista
+mv $HOME/lista $PREFIX/bin
+echo "[!] bghiti tkhdm tool kteb 'zxy' "
+echo "[!] bghiti tl3 wifiyat li deja ta7o kteb 'lista' "
+echo "[?] khsk chi7aja dozz   insta: @userr_ayoub" 
 # sf hada makan hhh
