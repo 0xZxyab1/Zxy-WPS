258dd0a5a2f3d14ffe9140a3312427a2ea46bbd9
Zxy Ayoub <155308552+0xZxyab1@users.noreply.github.com>
2024-12-12 12:27:24 +0100

    Add files via upload

--- banner.py
+++ banner.py
@@ -4,15 +4,15 @@ import random
 random_num = random.randint(1, 9)
 
 if (random_num == 1):
-	os.system("cat $HOME/Desktop/banners/1.txt")
+	os.system("cat $HOME/Zxy-WPS/banners/1.txt")
 if (random_num == 2):
-        os.system("cat $HOME/Desktop/banners/2.txt")
+        os.system("cat $HOME/Zxy-WPS/banners/2.txt")
 if (random_num == 3):
-        os.system("cat $HOME/Desktop/banners/3.txt")
+        os.system("cat $HOME/Zxy-WPS/banners/3.txt")
 if (random_num == 4):
-        os.system("cat $HOME/Desktop/banners/4.txt")
+        os.system("cat $HOME/Zxy-WPS/banners/4.txt")
 if (random_num == 5):
-        os.system("cat $HOME/Desktop/banners/5.txt")
+        os.system("cat $HOME/Zxy-WPS/banners/5.txt")
 if (random_num == 6):
-        os.system("cat $HOME/Desktop/banners/6.txt")
+        os.system("cat $HOME/Zxy-WPS/banners/6.txt")
 
